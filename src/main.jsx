f176ffa63b5a9cfa91c3cbf87acadfb2d73f693d
david <mario.bargianu@gmail.com>
2025-02-23 11:43:48 +0200

    change to h2

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// @ts-ignore
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
